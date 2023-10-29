6786a11420002b83af1441d8ade4b44c2270726d
donnie <kvognch@ya.ru>
2023-10-30 00:37:57 +0100

    5-4-0

---
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -0,0 +1,52 @@
+@if ($paginator->hasPages())
+    <nav>
+        <ul class="flex flex-wrap items-center justify-center gap-3">
+            {{-- Previous Page Link --}}
+            @if (!$paginator->onFirstPage())
+                <li>
+                    <a href="{{ $paginator->previousPageUrl() }}" rel="prev" class="block p-3 text-white hover:text-pink text-sm font-black leading-none" aria-label="{{ __('pagination.previous') }}">
+                        <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+                            <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
+                        </svg>
+                    </a>
+                </li>
+            @endif
+
+            {{-- Pagination Elements --}}
+            @foreach ($elements as $element)
+                {{-- "Three Dots" Separator --}}
+                @if (is_string($element))
+                    <li class="text-body/50 text-sm font-black leading-none" aria-disabled="true">{{ $element }}</li>
+                @endif
+
+                {{-- Array Of Links --}}
+                @if (is_array($element))
+                    @foreach ($element as $page => $url)
+                        @if ($page == $paginator->currentPage())
+                            <li class="active" aria-current="page">
+                                <span class="block p-3 pointer-events-none text-pink text-sm font-black leading-none">{{ $page }}</span>
+                            </li>
+                        @else
+                            <li>
+                                <a href="{{ $url }}" class="block p-3 text-white hover:text-pink text-sm font-black leading-none" aria-label="{{ __('Go to page :page', ['page' => $page]) }}">
+                                    {{ $page }}
+                                </a>
+                            </li>
+                        @endif
+                    @endforeach
+                @endif
+            @endforeach
+
+            {{-- Next Page Link --}}
+            @if ($paginator->hasMorePages())
+                <li>
+                    <a href="{{ $paginator->nextPageUrl() }}" rel="next" class="block p-3 text-white hover:text-pink text-sm font-black leading-none" aria-label="{{ __('pagination.next') }}">
+                        <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+                            <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+                        </svg>
+                    </a>
+                </li>
+            @endif
+        </ul>
+    </nav>
+@endif
